996f22eb4bb0a6784721c7404671d160b74e3cab
lucym19 <luciana.moraez09@gmail.com>
2023-01-16 19:35:03 -0300

    style

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML = days + "<span style='color:#9dddf9ff'>...</span>" +  hours + "<span style='color:#9dddf9ff'>...</span>"
+  + minutes + "<span style='color:#9dddf9ff'>...</span>"  + seconds;
 
   if (distance < 0) {
     clearInterval(x);
